604fe90a0b1bbafd0f664662fa7be8037198488b
Raymond <Raymond@Home>
2015-04-05 09:23:35 +0800

    added GameController

---
+++ src/main/java/com/creatingskies/game/editor/GameController.java
@@ -0,0 +1,69 @@
+package com.creatingskies.game.editor;
+
+import java.io.IOException;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
+import com.creatingskies.game.classes.TableViewController;
+import com.creatingskies.game.common.MainLayout;
+import com.creatingskies.game.core.Game;
+import com.creatingskies.game.core.GameDao;
+import com.creatingskies.game.model.IRecord;
+import com.creatingskies.game.model.user.User;
+
+public class GameController extends TableViewController{
+	
+	@FXML private TableView<Game> gamesTable;
+	@FXML private TableColumn<Game, String> titleColumn;
+	@FXML private TableColumn<Game, String> descriptionColumn;
+	@FXML private TableColumn<Game, String> typeColumn;
+	@FXML private TableColumn<Game, String> widthColumn;
+	@FXML private TableColumn<Game, String> heightColumn;
+	@FXML private TableColumn<User, Object> actionColumn;
+	
+	@FXML
+	@SuppressWarnings("unchecked")
+	public void initialize(){
+		super.initialize();
+		GameDao gameDao = new GameDao();
+		
+		titleColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getTitle()));
+		descriptionColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getDescription()));
+		typeColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getType().toString()));
+		widthColumn.setCellValueFactory(
+				cellData -> new SimpleStringProperty(String.valueOf(cellData.getValue().getHorizontalTiles().size())));
+		heightColumn.setCellValueFactory(
+				cellData -> new SimpleStringProperty(String.valueOf(cellData.getValue().getVerticalTiles().size())));
+		
+		actionColumn.setCellFactory(generateCellFactory(Action.DELETE, Action.EDIT, Action.VIEW));
+		gamesTable.setItems(FXCollections.observableArrayList(gameDao.findAllGames()));
+	}
+	
+	public void show(){
+		try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("Game.fxml"));
+            AnchorPane games = (AnchorPane) loader.load();
+            MainLayout.getRootLayout().setCenter(games);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+	
+	@Override
+	public TableView<? extends IRecord> getTableView() {
+		return gamesTable;
+	}
+
+	@Override
+	protected String getViewTitle() {
+		return "Games";
+	}
+
+}
